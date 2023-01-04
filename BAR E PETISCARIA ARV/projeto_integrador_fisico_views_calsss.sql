2f536c608c6781f66fd7ab00f776be215ad14520
Victor Hugo Marques <80768509+Victordelete@users.noreply.github.com>
2023-01-03 21:14:41 -0300

    Atualização insert
    Inserção dos exemplos das demais tabelas.
    
    Associação de foreign key de lista_compra e compras.

---
+++ BAR E PETISCARIA ARV/projeto_integrador_fisico_views_calsss.sql
@@ -0,0 +1,6 @@
+SELECT * FROM view_select_cliente_com_contatos;
+SELECT * FROM view_select_cliente_com_endereco;
+SELECT * FROM view_select_funcionario_com_contatos;
+SELECT * FROM view_select_funcionario_com_endereco;
+
+SELECT * FROM view_motos;

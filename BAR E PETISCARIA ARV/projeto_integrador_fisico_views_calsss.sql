fe3ca97970bf18851d9cecbfaffd5a340758f3cc
Victor Hugo Marques <80768509+Victordelete@users.noreply.github.com>
2023-01-12 19:47:29 -0300

    Exclusão arquivos temp

--- BAR E PETISCARIA ARV/projeto_integrador_fisico_views_calsss.sql
+++
@@ -1,6 +0,0 @@
-SELECT * FROM view_select_cliente_com_contatos;
-SELECT * FROM view_select_cliente_com_endereco;
-SELECT * FROM view_select_funcionario_com_contatos;
-SELECT * FROM view_select_funcionario_com_endereco;
-
-SELECT * FROM view_motos;
